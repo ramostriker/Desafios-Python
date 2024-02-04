2d6f4e8965ff4d7d5317f1a4367da841b5f09e48
Paulo Ramos <121035516+ramostriker@users.noreply.github.com>
2024-02-04 17:15:04 -0300

    Conversor de Moedas

---
+++ desafio010.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere US$1,00 = R$3,27.
+
+real = float(input('Quanto de dinheiro você tem na carteira? '))
+dolar = real / 3.27
+
+print('Com R${:.2f} você pode comprar US${:.2f}' .format(real, dolar))
