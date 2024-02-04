c4199166dd5356c829609acbbd4def19c404274c
Paulo Ramos <121035516+ramostriker@users.noreply.github.com>
2024-02-04 17:06:18 -0300

    Somando dois números

---
+++ desafio003.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois númeroe e mostre a soma entre eles
+
+num1 = int(input('Informe um número: '))
+num2 = int(input('Informe outro número: '))
+soma = num1+num2
+print('A soma entre {} e {} é igual a {}!' .format(num1, num2, soma))
